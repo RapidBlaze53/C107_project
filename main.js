bcc7dd3a921e5b9b446311bf44257462950a1d1c
RapidBlaze53 <127673703+RapidBlaze53@users.noreply.github.com>
2023-12-09 11:53:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var gotResults
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/nTFWUfCpc/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+}
